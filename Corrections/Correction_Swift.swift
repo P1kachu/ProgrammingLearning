259caca13304f9e9b0c705db83630c75461a51b2
imbavirus <jvdwpsp@gmail.com>
2015-10-29 18:31:44 +0200

    Proposed correction
    May not be 100% correct but as it currently is it compiles and displays output on http://swiftstub.com/
    
    - changed all println() to print()
    - fixed calculation functions to a point where they work
    - uncommented empty print() statements
    - replaced do while with repeat while

--- Corrections/Correction_Swift.swift
+++ Corrections/Correction_Swift.swift
@@ -8,7 +8,6 @@
 //      - No one yet.
 //
 
-import Foundation
 
 // Part One:
 
@@ -20,14 +19,14 @@ var name:String = "Stan"
 
 // FIXME: Print
 // Print the following sentence in the console "You are NAME and you are AGE years old !". Don't forget to add a newline at the end
-println("You are \(name) and you are \(age) years old !") //or
+print("You are \(name) and you are \(age) years old !") //or
 print("You are \(name) and you are \(age) years old ! \n")
 
 // FiXME: Concatenation
 // Create a new string variable called "hello" which value is "Hello ". Add "name" at the end of "hello" (Concatenation) then print it
 var hello:String = "Hello "
 hello += name
-println(hello)
+print(hello)
 
 // FIXME: Array
 // create a new string array called "shoppingList", with three elements of your choice. Create an int variable containing the number of
@@ -39,20 +38,20 @@ var nbOfElemts:Int = shoppingList.count
 // Create a simple for-loop for an integer "i" going from 1 to 10 that print the value of "i"
 for i in 1...10
 {
-    println(i)
+    print(i)
 }
 
 // FIXME: For-loop - shoppingList
 // Create a for loop that iterate through "shoppingList" and prints each element with "You have to buy (elemt)".
 for j in 0..<nbOfElemts
 {
-    println("You have to buy \(shoppingList[j])")
+    print("You have to buy \(shoppingList[j])")
 }
 // FIXME: Foreach-loop
 // Do the same with a foreach-loop.
 for elemt in shoppingList // In Swift, foreach-loops ar for[...]in loops.
 {
-    println("You have to buy \(elemt)")
+    print("You have to buy \(elemt)")
 }
 
 // FIXME: If-statement
@@ -62,11 +61,11 @@ for i in 1...10
 {
     if i%2 == 0
     {
-        println("\(i) is even")
+        print("\(i) is even")
     }
     else
     {
-        println("\(i) is odd")
+        print("\(i) is odd")
     }
 }
 
@@ -79,24 +78,24 @@ for elemt in shoppingList
 {
     if elemt == element
     {
-        println("You have to buy \(elemt)!")
+        print("You have to buy \(elemt)!")
         break
     }
     else
     {
-        println("Nope, you don't need \(elemt)")
+        print("Nope, you don't need \(elemt)")
     }
 }
 //You can also do it without looping with the "contains" function:
 
-if contains(shoppingList,element)
+/*if (contains(shoppingList,element))
 {
-    println("You have to buy \(element)!")
+    print("You have to buy \(element)!")
 }
 else
 {
-    println("Nope, you don't need \(element)")
-}
+    print("Nope, you don't need \(element)")
+}*/
 
 
 
@@ -111,11 +110,11 @@ else
 // Don't forget to call the function !
 func TriForce() -> Void // When returning void, you can just write func TriForce(){ // Do stuff } without returning type
 {
-    println("   /\\  ")
-    println("  /__\\ ")
-    println(" /\\  /\\  ")
-    println("/__\\/__\\ ")
-    println("TRIFORCE")
+    print("   /\\  ")
+    print("  /__\\ ")
+    print(" /\\  /\\  ")
+    print("/__\\/__\\ ")
+    print("TRIFORCE")
 }
 TriForce() //Calling the function
 
@@ -126,13 +125,14 @@ func Hello(yourName:String) -> String
 {
     return  "Hello \(yourName)!"
 }
-println(Hello("Stan"))
+print(Hello("Stan"))
 
 
 // FIXME: Functions - Multiple parameters
 // Create a function that takes two integers as parameters and returns the addition of these two.
 // You can do the same with multiplication, subtraction and division.
-func Addition(a:Int, b:Int) -> Int
+
+func Addition(a: Int, b: Int) -> Int
 {
     return a + b
 }
@@ -148,26 +148,26 @@ func Division(a:Int, b:Int) -> Int
 {
     return a / b
 }
-println(Addition(5, 12))
-println(Subtraction(5, 12))
-println(Multiplication(5, 12))
-println(Division(5, 12)) // returns 0 because "a" and "b" are Integers
-println(Division(10, 2))
+print(Addition(5, b: 12))
+print(Subtraction(5, b: 12))
+print(Multiplication(5, b: 12))
+print(Division(5, b: 12)) // returns 0 because "a" and "b" are Integers
+print(Division(10, b: 2))
 
 
 // FIXME: User entry
 // Create a string variable that takes what the user enter in the console as value. Then print "You entered (value of string)"
 // At this point, the Swift programming language doessn't have any easy function to get the user input,
 // so you may skip this one. Here is a way to do it (Works in XCode6b5):
-func input() -> String
+/*func input() -> String
 {
     var keyboard = NSFileHandle.fileHandleWithStandardInput()
     var inputData = keyboard.availableData
     return NSString(data: inputData, encoding:NSUTF8StringEncoding)
 }
 var userInput:String = input()
-println("Uou entered \(userInput)")
-
+print("Uou entered \(userInput)")
+*/
 
 // FIXME: While loop
 // Create a while loop that takes a number and divides it by 2 until it is less than 3
@@ -178,17 +178,17 @@ while number > 3
     // can be done with number /= 2
     print("\(number) ")
 }
-println()
+//print()
 
 
 // FIXME: do-While loop
 // Do the same with a do-while loop
 var number2 = 526
-do{
+repeat{
     number2 /= 2
     print("\(number2) ")
 } while number2 > 3;
-println()
+//print()
 
 
 
@@ -208,9 +208,9 @@ func rdmBounds(a:UInt32, b:UInt32) -> Int
 }
 for i in 0...10
 {
-    print("\(rdmBounds(0, 10)) ")
+    print((rdmBounds(0, b: 10)))
 }
-println()
+//print()
 
 
 // FIXME: Multi-dimensionnal array
@@ -226,7 +226,7 @@ for i in 0..<multiArray.count
 {
     for j in 0..<multiArray[i].count
     {
-        multiArray[i][j] = rdmBounds(1, 10)
+        multiArray[i][j] = rdmBounds(1, b: 10)
     }
 }
 
@@ -237,7 +237,7 @@ for elemnt in multiArray
     {
         print("\(numbers),")
     }
-    println("},")
+    print("},")
 }
 
 
