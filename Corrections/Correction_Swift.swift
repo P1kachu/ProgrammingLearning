c57716bd43dba5f5cbe7edc8dcada781073e90ae
imbavirus <jvdwpsp@gmail.com>
2015-10-29 20:07:10 +0200

    Update Correction_Swift.swift

--- Corrections/Correction_Swift.swift
+++ Corrections/Correction_Swift.swift
@@ -5,7 +5,7 @@
 //  Created by P1kachu on 14/09/14.
 //
 //  Reviewed by:
-//      - No one yet.
+//      - Imbavirus (Compiles and outputs fine on swiftstub.com, not 100% tested but made some corrections) on 29/10/15
 //
 
 // import Foundation
@@ -13,49 +13,41 @@
 // Part One:
 
 
-// FIXME: Declare variables
 // Declare two variables: an integer named "age", and a string named "name" with corresponding values (your name and age)
 var age:Int = 19
 var name:String = "Stan"
 
-// FIXME: Print
 // Print the following sentence in the console "You are NAME and you are AGE years old !". Don't forget to add a newline at the end
-print("You are \(name) and you are \(age) years old !") //or
 print("You are \(name) and you are \(age) years old ! \n")
 
-// FiXME: Concatenation
 // Create a new string variable called "hello" which value is "Hello ". Add "name" at the end of "hello" (Concatenation) then print it
 var hello:String = "Hello "
 hello += name
 print(hello)
 
-// FIXME: Array
 // create a new string array called "shoppingList", with three elements of your choice. Create an int variable containing the number of
 // elements in "shoppingList" (using a function of the array/using the array)
 var shoppingList:[String] = ["some milk", "a Chevy Camaro", "a life"]
 var nbOfElemts:Int = shoppingList.count
 
-// FIXME: For-loop - Integer
 // Create a simple for-loop for an integer "i" going from 1 to 10 that print the value of "i"
 for i in 1...10
 {
     print(i)
 }
 
-// FIXME: For-loop - shoppingList
 // Create a for loop that iterate through "shoppingList" and prints each element with "You have to buy (elemt)".
 for j in 0..<nbOfElemts
 {
     print("You have to buy \(shoppingList[j])")
 }
-// FIXME: Foreach-loop
+
 // Do the same with a foreach-loop.
 for elemt in shoppingList // In Swift, foreach-loops ar for[...]in loops.
 {
     print("You have to buy \(elemt)")
 }
 
-// FIXME: If-statement
 // Modify the first for-loop (with i from 1 to 10) such that it prints "(value of i) is even" when "i" is divisible
 // by 2 (You may want to learn more about "modulo" (%)). Else, print "(value of i) is odd".
 for i in 1...10
@@ -70,7 +62,6 @@ for i in 1...10
     }
 }
 
-// FIXME: Sum Up
 // Create a string variable called "element" with the value of your choice. Then create a for-loop/foreach-loop that checks if "shoppingList"
 // contains "element". If yes, print "You have to buy (value of element) !", and stop the loop (search how to stop a loop).
 // If not, print "Nope, you don't need (value of "element")".
@@ -87,6 +78,8 @@ for elemt in shoppingList
         print("Nope, you don't need \(elemt)")
     }
 }
+
+//FIXME: "contains" function missing (could be in included file but couldn't test that on SwiftStub
 //You can also do it without looping with the "contains" function:
 
 /*if (contains(shoppingList,element))
@@ -105,7 +98,6 @@ else
 // Part Two:
 
 
-// FIXME: Functions - Ascii
 // Create a function that returns nothing and which doesn't takes any parameter. It should just be named "TriForce"
 // and print the TriForce symbol (one triangle over two other ones, can be found on internet) with "TRIFORCE"
 // Don't forget to call the function !
@@ -120,7 +112,6 @@ func TriForce() -> Void // When returning void, you can just write func TriForce
 TriForce() //Calling the function
 
 
-// FIXME: Functions - One parameter
 // Create a function that takes a string as parameter and returns "Hello (value of string) !"
 func Hello(yourName:String) -> String
 {
@@ -129,7 +120,6 @@ func Hello(yourName:String) -> String
 print(Hello("Stan"))
 
 
-// FIXME: Functions - Multiple parameters
 // Create a function that takes two integers as parameters and returns the addition of these two.
 // You can do the same with multiplication, subtraction and division.
 
@@ -170,7 +160,6 @@ var userInput:String = input()
 print("Uou entered \(userInput)")
 */
 
-// FIXME: While loop
 // Create a while loop that takes a number and divides it by 2 until it is less than 3
 var number = 57
 while number > 3
@@ -181,11 +170,9 @@ while number > 3
 }
 //print()
 
-
-// FIXME: do-While loop
 // Do the same with a do-while loop
 var number2 = 526
-repeat{
+repeat{  //Do..while is repeat..while in swift
     number2 /= 2
     print("\(number2) ")
 } while number2 > 3;
