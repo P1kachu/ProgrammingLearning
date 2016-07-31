004f66e69429bd06f5d35ea03bad49ce71cbb377
PraveenKishore <praveenkishorepraveen@gmail.com>
2016-07-31 20:31:39 +0530

    Added more solutions to JavaCorrectionAdvanced.java

--- Corrections/JavaCorrectionAdvanced.java
+++ Corrections/JavaCorrectionAdvanced.java
@@ -16,6 +16,7 @@ class JavaCorrectionAdvanced
     {
 		// Microsoft Interview Question
 		System.out.println(reverseWordOrder("This is a Cat"));
+		System.out.println("");
 		
 		// Facebook Interview Question 1
 		if(isPalindrome("Malayalam")) {
@@ -23,6 +24,7 @@ class JavaCorrectionAdvanced
 		} else {
 			System.out.println("Not a palindrome");
 		}
+		System.out.println("");
 		
 		// Facebook Interview Question 2
 		if(facebook2(new int[] { 1, 12, 11, 3, 4})) {	// Returns true if 2 or more numbers is 12
@@ -30,6 +32,7 @@ class JavaCorrectionAdvanced
 		} else {
 			System.out.println("Two or more numbers is not 12");
 		}
+		System.out.println("");
 		
 		// Microsoft Interview Question
 		int a[][] = new int[][]{{  1,  2,  3,  4 }, 
@@ -37,6 +40,44 @@ class JavaCorrectionAdvanced
 								{  9, 10,  0, 11 }, 
 								{ 12, 13, 14, 15 }};
 		replaceWith0(a);
+		System.out.println("");
+		
+		// HackerRank Data Structure & Algorithm Questions 
+		// You are given an array of integers of size N . You need to print  
+		// the sum of the elements of the array.  
+		int sum = 0;
+		int num[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		for(int i = 0; i < num.length; i++) {
+			sum += num[i];
+		}
+		System.out.println("Sum: " + sum);
+		System.out.println("");
+		
+		// HackerRank Data Structure & Algorithm Questions: 
+		// Your teacher has given you the task to draw the structure of a staircase.  
+		// Being an expert programmer, you decided to make a program for the same.  
+		// You are given the height of the staircase. You need to print a  
+		// staircase as shown in the example.  
+		// Output:  
+		//      # 
+		//     ## 
+		//    ### 
+		//   #### 
+		//  ##### 
+		// ###### 
+		int height = 6;
+		int s = height-1, h = 1;	// s: For storing number of space to be printed next, h for storing number of '#' to be printed next
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < s; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k < h; k++) {
+				System.out.print("#");
+			}
+			s--;
+			h++;
+			System.out.println("");
+		}
 	}
 	
 	static String reverseWordOrder(String input) {
@@ -125,6 +166,7 @@ class JavaCorrectionAdvanced
 		}
 		
 		// Print the matrix
+		System.out.println("Output: ");
 		for(int i = 0; i < a.length; i++) {
 			for(int j = 0; j < a[0].length; j++) {
 					System.out.print(a[i][j] + "\t");
